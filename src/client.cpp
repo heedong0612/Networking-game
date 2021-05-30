4ecccdf23a2334ba5df589ac994231cab9245e52
Donghee Lee <heedong@uw.edu>
2021-05-30 06:34:20 -0700

    add client functions

---
+++ src/client.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <string>
+#include <time.h>
+//#include <NetworkAPI>
+
+using namespace std;
+
+enum Player {p0, p1};
+enum Button {b0, b1, empty};
+
+class Client {
+public:
+	Client();
+	void registerUser(string username);
+	bool chooseMove(int r, int c);
+	bool isGameOver();
+	bool isMyTurn();
+	bool listenForServer();		
+	void endGame();
+
+private:
+	string username;
+	string pid_str;			// player id either "p0" or "p1"
+	int pid;
+	bool isGameOver;
+	bool iWon;
+	bool myTurn;
+	
+	int row;
+	int col;
+	int board[6][7];		
+
+	NetworkAPI nAPI;
+};
+
+Client::Client() {
+
+	row = 6;
+	col = 7;
+
+	// initialize an empty board
+	for (int r = 0; r < row; r++) {
+		for (int c = 0; c < col; c++) {
+			board[r][c] = empty;
+		}
+	}
+
+	char hostname [] = "placeholder";
+	
+	nAPI = NetworkAPI();
+	nAPI.setup4Client(hostname);
+
+	isGameOver = false;
+}
+
+void Client::registerUser(string username) {
+	username = username;
+}
+
+// the message from server can either be "start", "gameover", or "button" message
+bool Client::listenForServer() {
+	string message = nAPI.listenFromServer();
+
+	if (message.length() > 5 && message.substr(0,5) == "start") {		
+		
+		// check if this player is starting first
+		myTurn = message.substr(6,3) == "yes";
+
+		// register player id
+		pid_str = message.substr(message.length() - 2);
+		if (pid_str == "p0") pid = p0;
+		else pid = p1;
+
+	} else if (message.length() > 8 && message.substr(0,8) == "gameover") {
+		// check if this player won the game
+		isGameOver = true;
+		iWon = (message.substr(9,2) == pid_str);
+
+	} else if (message.length() > 6 && message.substr(0,6) == "button") {
+		// update my board with the new move ack'ed by the server
+		string player = message.substr(7,2);
+		int r = (int)message[10] - 48;
+		int c = (int)message[12] - 48;
+
+		if (player == pid_str) {	// this player's recent move got ACK'd
+			// update my board
+			board[r][c] = pid;
+
+		} else {					// other player has made a move
+			board[r][c] = (1 - pid);
+			myTurn = true;
+		}
+		
+	}
+}
+
+// check and return whether r, c are valid moves -- do not update board here. update it when the server ACK's it 
+bool Client::chooseMove(int r, int c) {
+	
+	// check if r, c are valid
+	if (r < 0 || r >= row || c < 0 || c >= col) return false;
+	if (board[r][c] != empty) return false;
+
+	// send the moves to the server
+	const char message = "move " + pid_str + " " + r + " " + c;
+	nAPI.sendToServer(message);
+	myTurn = false;
+
+}
+
+bool Client::isMyTurn() {
+	return myTurn;
+}
+
+bool Client::isGameOver() {
+	return isGameOver;
+}
+
+int main(){
+	
+	Client client = Client();
+	
+	// FIX: accept user input for user name
+	string myName = "Donghee"; // placeholder 
+	client.registerUser(myName);
+
+	whlie (!client.isGameOver()) {
+		client.listenForServer(); 
+		if (client.isMyTurn()) {
+
+			// FIX: accept user input r, c
+
+			while (!client.chooseMove(r, c)) {	// keep asking for r, c if they were invalid choices
+				// prompt for user input again
+			}
+		}
+	}
+
+	client.endGame();
+	
+}
